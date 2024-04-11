f2f76f4f7f8c47867291dffc6204dadbfad6747f
Murilo <dixmurilox@gmail.com>
2024-04-11 11:38:30 -0300

    feat: funcionalidade calc

--- calculator.js
+++ calculator.js
@@ -13,4 +13,30 @@ function mult(v1, v2) {}
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
 
-console.log(sum(v1, v2));
+
+console.log('Calculadora');
+console.log('1 - Soma');
+console.log('2 - Subtração');
+console.log('3 - Divisão');
+console.log('4 - Multiplicação');
+console.log('5 - Raiz quadrada');
+const option = prompt('Escolha uma opção: ');
+
+if (option === '5') {
+  const value = Number(prompt('Digite um número: '));
+  console.log(`O resultado é: ${square(value)}`);
+} else {
+  const value1 = Number(prompt('Digite o primeiro número: '));
+  const value2 = Number(prompt('Digite o segundo número: '));
+  if (option === '1') {
+    console.log(`O resultado é: ${sum(value1, value2)}`);
+  } else if (option === '2') {
+    console.log(`O resultado é: ${sub(value1, value2)}`);
+  } else if (option === '3') {
+    console.log(`O resultado é: ${div(value1, value2)}`);
+  } else if (option === '4') {
+    console.log(`O resultado é: ${mult(value1, value2)}`);
+  } else {
+    console.log('Opção inválida');
+  }
+}
