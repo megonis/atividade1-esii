0d9e6e5962b20f6a53ac45e274d005302b7a7045
Murilo <dixmurilox@gmail.com>
2024-04-14 20:24:20 -0300

    feat: adicionando função para calcular com base na escolha do usuario

--- calculator.js
+++ calculator.js
@@ -14,35 +14,51 @@ function mult(v1, v2) {}
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
 
-while (true) {
-  //  Calculadora
+// Função para calcular com base na opção selecionada
+function calculate(option, value1, value2) {
+  switch (option) {
+    case 1:
+      return sum(value1, value2);
+    case 2:
+      return sub(value1, value2);
+    case 3:
+      return div(value1, value2);
+    case 4:
+      return mult(value1, value2);
+    case 5:
+      return square(value1);
+    default:
+      return "Digite um valor válido"
+  }
+}
+
+do {
+  // Exibe as opções para o usuário
   console.log("Calculadora");
   console.log("1 - Soma");
   console.log("2 - Subtração");
   console.log("3 - Divisão");
   console.log("4 - Multiplicação");
   console.log("5 - Raiz quadrada");
-  console.log("6 - Sair");
-  const option = prompt("Escolha uma opção: ");
-  //  Executa a opção escolhida pelo usuário
-  if (option === "5") {
+  console.log("0 - Sair");
+  // Recebe a opção do usuário
+  const option = Number(prompt("Escolha uma opção: "));
+
+  // Verifica se a opção é válida
+  if (isNaN(option) || option < 0 || option > 5) {
+    console.log("Opção inválida");
+    continue;
+  }
+
+  // Verifica se a opção é para a raiz quadrada
+  if (option === 5) {
     const value = Number(prompt("Digite um número: "));
-    console.log(`O resultado é: ${square(value)}`);
+    console.log(`O resultado é: ${calculate(option, value)}`);
+  } else if (option === 0) {
+    break;
   } else {
     const value1 = Number(prompt("Digite o primeiro número: "));
     const value2 = Number(prompt("Digite o segundo número: "));
-    if (option === "1") {
-      console.log(`O resultado é: ${sum(value1, value2)}`);
-    } else if (option === "2") {
-      console.log(`O resultado é: ${sub(value1, value2)}`);
-    } else if (option === "3") {
-      console.log(`O resultado é: ${div(value1, value2)}`);
-    } else if (option === "4") {
-      console.log(`O resultado é: ${mult(value1, value2)}`);
-    } else if (option === "6") {
-      break;
-    } else {
-      console.log("Opção inválida");
-    }
+    console.log(`O resultado é: ${calculate(option, value1, value2)}`);
   }
-}
+} while (true);
